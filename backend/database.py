76a8aec17742c70a9fb63695b7b5b6d0b905ea9b
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-06-05 18:57:58 +0530

    render database

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,20 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# replace 'u' and 'p' with pgSQL user and pass
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Priyanshu1606@localhost:5432/mindmesh"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
